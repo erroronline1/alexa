485c9b040ff6b445f15838a2a1ca371bded81764
error on line 1 <dev@erroronline.one>
2020-07-17 23:22:01 +0200

    finally applied framework to first skill

--- chefsekretaerin.php
+++ chefsekretaerin.php
@@ -2,112 +2,84 @@
 
 include ('nonpublic.php'); // application ids, database connections, etc.
 
-if ($ALEXA->verified($post, $rawpost, $ChefsekretaerinAppId)){
+if ($ALEXA->verified($ChefsekretaerinAppId)){
 
-$boss=["der boss","der könig","die königin","der chef","die chefin","der bestimmer","die bestimmerin"];
-$wer=$post->request->intent->slots->NAME->value;
-$wo=$post->request->intent->slots->WHERE->value;
-$was=$post->request->intent->slots->BOSS->value;
-$shoudEndSession=true;
-$output="hallo";
+    $boss = ["der boss", "der könig", "die königin", "der chef", "die chefin", "der bestimmer", "die bestimmerin"];
+    $wer = $ALEXA->post->request->intent->slots->NAME->value;
+    $wo = $ALEXA->post->request->intent->slots->WHERE->value;
+    $was = $ALEXA->post->request->intent->slots->BOSS->value;
+    $userID = $ALEXA->post->session->user->userId;
 
-$tchef=$ChefsekretaerinMysqli->query("SELECT name FROM wer_ist_der_chef WHERE amazon_id='".$post->session->user->userId."' LIMIT 1")->fetch_assoc();
+    $tchef = $ChefsekretaerinMysqli->query("SELECT name FROM wer_ist_der_chef WHERE amazon_id='" . $userID . "' LIMIT 1")->fetch_assoc();
 
-$chef=$tchef['name']?$tchef['name']:'noch niemand';
+    $chef = $tchef['name'] ? $tchef['name'] : 'noch niemand';
 
-if ($post->request->type=="IntentRequest"){
-    if ($post->request->intent->name=="DEFAULT"){
-        if (in_array($was,$boss)) $output=$chef.' ist '.$wo.' '.$was;
-    else $output='du bist vielleicht '.$wo.' '.$was.', aber '.$chef.' ist hier der chef';
+    if ($ALEXA->post->request->type == "LaunchRequest"){
+        $OUTPUT->speak = 'frag mich wer hier der chef ist. oder nach hilfe.';
+        if (!$tchef['name']) $OUTPUT->speak .= ' ich weiß aber noch nicht wer hier der chef ist. damit ich mir das merke, sag etwas wie: ich bin alexa und ich bin der chef.';
+        $OUTPUT->reprompt = 'frag mich wer der chef ist';
     }
-    elseif ($post->request->intent->name=="NAME_REQUEST"){
-        if ($wer==$chef && in_array($was,$boss)) $output='ja! ja, '.$wer.' ist '.$wo.' '.$was;
-        elseif ($wer!=$chef && in_array($was,$boss)) $output='nein, '.$wer.' ist '.$wo.' nicht '.$was.', das ist '.$chef;
-        elseif ($wer==$chef && !in_array($was,$boss)) $output='nein, nein! '.$chef.' ist '.$wo.' der chef';
-        else $output='kann sein, dass '.$wer.' '.$wo.' '.$was.' ist, aber '.$chef.' ist hier der chef';
+    elseif ($ALEXA->post->request->type == 'SessionEndedRequest') {
+        $OUTPUT->speak = 'dann bis bald. ich hoffe ich konnte helfen';
     }
-    elseif ($post->request->intent->name=="SELF_REQUEST"){
-        if ($post->session->attributes->sessionWer) {
-            if ($post->session->attributes->sessionWer==$chef && in_array($was,$boss)) $output='ja '.$chef.', du bist '.$wo.' '.$was;
-            elseif ($post->session->attributes->sessionWer==$chef && !in_array($was,$boss)) $output='nein '.$chef.', du bist hier der chef';
-            elseif ($post->session->attributes->sessionWer!=$chef && in_array($was,$boss)) $output='nein, das bist du nicht, das ist '.$chef;
-            else $output='das kann schon sein, aber so gut kennen wir uns ja auch nicht';
+    elseif ($ALEXA->post->request->type == "IntentRequest"){
+        if ($ALEXA->IntentName == "DEFAULT"){
+            if (in_array($was, $boss)) $OUTPUT->speak = $chef . ' ist ' . $wo . ' ' . $was;
+            else $OUTPUT->speak = 'du bist vielleicht ' . $wo . ' ' . $was . ', aber ' . $chef . ' ist hier der chef';
         }
-        else { $output='wie heißt du denn?';
-            $sessionAttributes=['sessionWas'=>$was,'sessionWo'=>$wo];
-            $shoudEndSession=false;
+        elseif ($ALEXA->IntentName == "NAME_REQUEST"){
+            if ($wer == $chef && in_array($was, $boss)) $OUTPUT->speak = 'ja! ja, ' . $wer . ' ist ' . $wo . ' ' . $was;
+            elseif ($wer != $chef && in_array($was, $boss)) $OUTPUT->speak = 'nein, ' . $wer . ' ist ' . $wo . ' nicht ' . $was . ', das ist ' . $chef;
+            elseif ($wer == $chef && !in_array($was, $boss)) $OUTPUT->speak = 'nein, nein! ' . $chef . ' ist ' . $wo . ' der chef';
+            else $OUTPUT->speak = 'kann sein, dass ' . $wer . ' ' . $wo . ' ' . $was . ' ist, aber ' . $chef . ' ist hier der chef';
+        }
+        elseif ($ALEXA->IntentName == "SELF_REQUEST"){
+            if ($ALEXA->post->session->attributes->sessionWer) {
+                if ($ALEXA->post->session->attributes->sessionWer == $chef && in_array($was, $boss)) $OUTPUT->speak = 'ja ' . $chef . ', du bist ' . $wo . ' ' . $was;
+                elseif ($ALEXA->post->session->attributes->sessionWer == $chef && !in_array($was, $boss)) $OUTPUT->speak = 'nein ' . $chef . ', du bist hier der chef';
+                elseif ($ALEXA->post->session->attributes->sessionWer != $chef && in_array($was, $boss)) $OUTPUT->speak = 'nein, das bist du nicht, das ist ' . $chef;
+                else $OUTPUT->speak = 'das kann schon sein, aber so gut kennen wir uns ja auch nicht';
+            }
+            else {
+                $OUTPUT->speak = $OUTPUT->reprompt = 'wie heißt du denn?';
+                $OUTPUT->sessionAttributes = ['sessionWas' => $was, 'sessionWo' => $wo];
+            }
+        }
+        elseif ($ALEXA->IntentName == "SELF_REQUEST_SET_NAME"){
+            if ($ALEXA->post->session->attributes->sessionWas) {
+                $was = $ALEXA->post->session->attributes->sessionWas;
+                if ($wer == $chef && in_array($was, $boss)) $OUTPUT->speak = 'ja ' . $chef . ', du bist ' . $wo . ' ' . $was;
+                elseif ($wer == $chef && !in_array($was, $boss)) $OUTPUT->speak = 'nein ' . $chef . ', du bist hier der chef';
+                elseif ($wer != $chef && in_array($was, $boss)) $OUTPUT->speak = 'nein, du bist nicht ' . $was . ', das ist ' . $chef;
+                else $OUTPUT->speak = 'das kann schon sein, aber so gut kennen wir uns ja auch nicht';
+            }
+            else {
+                $OUTPUT->speak = 'hallo ' . $wer . ', frag mich ob du der chef bist';
+                $OUTPUT->reprompt = 'frag mich wer der chef ist';
+            }
+            $OUTPUT->sessionAttributes = ['sessionWer' => $wer];
+        }
+        elseif ($ALEXA->IntentName == "SELF_REQUEST_STORE_NAME"){
+            if ($wer && in_array($was, $boss)) {
+                $ChefsekretaerinMysqli->query("INSERT INTO wer_ist_der_chef VALUES ('" . $userID . "','" . $wer . "') ON DUPLICATE KEY UPDATE name='" . $wer . "'");
+                $OUTPUT->speak = 'hallo ' . $wer . ', es freut mich dich kennen zu lernen';
+                $OUTPUT->card->title = 'Wer ist hier der Chef?';
+                $OUTPUT->card->text = $wer . ' ist hier jetzt offiziell ' . $was . ' ' . $ChefsekretaerinMysqli->error;
+            }
+            else {
+                $OUTPUT->speak = 'ich habe dich nicht verstanden. versuchs nochmal';
+                $OUTPUT->reprompt = 'ich habe dich nicht verstanden. sage mir wer der chef ist oder frage nach hilfe';
+            }
+        }
+        elseif ($ALEXA->IntentName == "AMAZON.HelpIntent"){
+            $OUTPUT->speak=$OUTPUT->reprompt = 'ich kann dir sagen, wer hier der chef ist. oder die königin. oder der bestimmer. frag einfach. damit ich mir das merke, sag etwas wie: ich bin alexa und ich bin der chef.';
+            $OUTPUT->card->title = 'Bestimme wer der Chef ist.';
+            $OUTPUT->card->text= ($chef ? 'Zur Zeit ist ' . ucfirst($chef) . ' hier der Chef. Wenn Du das ändern möchtest s' : 'S').'ag "Ich bin {Name} und ich bin der Chef".';
+        }
+        elseif ($ALEXA->IntentName == "AMAZON.StopIntent" || $ALEXA->IntentName == "AMAZON.CancelIntent"){
+            $OUTPUT->speak = 'dann bis bald. ich hoffe ich konnte helfen';
         }
     }
-    elseif ($post->request->intent->name=="SELF_REQUEST_SET_NAME"){
-        if ($post->session->attributes->sessionWas) {
-            $was=$post->session->attributes->sessionWas;
-            if ($wer==$chef && in_array($was,$boss)) $output='ja '.$chef.', du bist '.$wo.' '.$was;
-            elseif ($wer==$chef && !in_array($was,$boss)) $output='nein '.$chef.', du bist hier der chef';
-            elseif ($wer!=$chef && in_array($was,$boss)) $output='nein, du bist nicht '.$was.', das ist '.$chef;
-            else $output='das kann schon sein, aber so gut kennen wir uns ja auch nicht';
-        }
-        else {
-            $output='hallo '.$wer.', frag mich ob du der chef bist';
-            $shoudEndSession=false;
-            $reprompt=['outputSpeech'=>['type'=>'PlainText', 'text'=>'frag mich wer der chef ist']];
-        }
-        $sessionAttributes=['sessionWer'=>$wer];
-    }
-    elseif ($post->request->intent->name=="SELF_REQUEST_STORE_NAME"){
-        if ($wer && in_array($was,$boss)) {
-        $ChefsekretaerinMysqli->query("INSERT INTO wer_ist_der_chef VALUES ('".$post->session->user->userId."','".$wer."') ON DUPLICATE KEY UPDATE name='".$wer."'");
-        $output='hallo '.$wer.', es freut mich dich kennen zu lernen';
-        $card=['type'=>'Simple',
-            'title'=>'Wer ist hier der Chef?',
-            'content'=>$wer. ' ist hier jetzt offiziell '.$was.' '.$ChefsekretaerinMysqli->error
-        ];
-    }
-        else { $output='ich habe dich nicht verstanden. versuchs nochmal'; $shoudEndSession=false; $reprompt=['outputSpeech'=>['type'=>'PlainText', 'text'=>'ich habe dich nicht verstanden. sage mir wer der chef ist oder frage nach hilfe']];
-        }
-    }
-    elseif ($post->request->intent->name=="AMAZON.StopIntent"){
-        $output='dann bis bald. ich hoffe ich konnte helfen';
-    }
-    elseif ($post->request->intent->name=="AMAZON.HelpIntent"){
-        $output='ich kann dir sagen, wer hier der chef ist. oder die königin. oder der bestimmer. frag einfach. damit ich mir das merke, sag etwas wie: ich bin alexa und ich bin der chef.';
-        $card=['type'=>'Simple',
-            'title'=>'Bestimme wer der Chef ist.',
-            'content'=>($chef?'Zur Zeit ist '.ucfirst($chef).' hier der Chef. Wenn Du das ändern möchtest s':'S').'ag "Ich bin {Name} und ich bin der Chef".'
-        ];
-        $shoudEndSession=false;
-    }
-    elseif ($post->request->intent->name=="AMAZON.CancelIntent"){
-        $output='dann bis bald. ich hoffe ich konnte helfen';
-    }
-
-}
-elseif ($post->request->type=="LaunchRequest"){
-    $output='frag mich wer hier der chef ist. oder nach hilfe.';
-    if (!$tchef['name']) $output.=' ich weiß aber noch nicht wer hier der chef ist. damit ich mir das merke, sag etwas wie: ich bin alexa und ich bin der chef.';
-    $shoudEndSession=false;
-    $reprompt=['outputSpeech'=>['type'=>'PlainText', 'text'=>'frag mich wer der chef ist']];
-}
-elseif ($post->request->type== 'SessionEndedRequest') {
-    $output='dann bis bald. ich hoffe ich konnte helfen';
-}
-
-    $responseArray = [
-        'version' => '1.0',
-        'response' => [
-              'outputSpeech' => [
-                    'type' => 'PlainText',
-                    'text' => $output
-              ],
-             'card'=>$card,
-             'reprompt'=>$reprompt,
-             'shouldEndSession' => $shoudEndSession
-            ],
-        'sessionAttributes'=>$sessionAttributes
-    ];
-    header('Content-Type: application/json;Content-Length:'.strlen(json_encode($responseArray)));
-    echo json_encode($responseArray);
-
-
-
+    $OUTPUT->answer();
 } else $ALEXA->verificationfailed();
 ?>
--- readme.md
+++ readme.md
@@ -3,13 +3,13 @@ amazon alexa skills. more or less useful
 
 i do them in php for the easiest implementation on my webspace.
 in the beginning i had only one ssl certificate, therefore the unusual hostname in former versions and the media-file-workaround within the skills endpoints. i could resolve the latter, but it may as well serve as an example.
-playing with alexa once in a while i built a small framework (that most possibly evolves with every new skill) to be able to concentrate on the specific tasks of the skill.
+playing with alexa once in a while i built a ~~small~~ framework (that most possibly evolves with every new skill) to be able to concentrate on the specific tasks of the skill.
 skills that respond or are distributed in german only still try to have english code that still might be comprehensible.
 
 feel free to share your opinion and improvements.
 
 ## chefsekretaerin
-<img src="assets/chefsekretaerin108.png" alt="chefsekretaerin skill logo" style="float:left; height:50px; padding-right:1em;" /> *Wer ist hier der Chef?* is my first skill where one can ask who makes the rules. the name can be set. it contains session-variables, storing and fetching database information on the endpoint. the use of the alexa-libray was implemented later, for some reason it was not fully possible, but it works now.
+<img src="assets/chefsekretaerin108.png" alt="chefsekretaerin skill logo" style="float:left; height:50px; padding-right:1em;" /> *Wer ist hier der Chef?* is my first skill where one can ask who makes the rules. the name can be set. it contains session-variables, storing and fetching database information on the endpoint.
 
 no special interface settings needed
 
@@ -33,6 +33,6 @@ no special interface settings needed
 [live skill](https://www.amazon.de/dp/B07Q1C8Z61)
 
 ## forgetmenot
-<img src="assets/forgetmenot108.png" alt="remind me skill logo" style="float:left; height:50px; padding-right:1em;" /> *forget-me-not / vergissmeinnicht* might actually be useful since it can remind you regulary for custom things. it fights with some silly amazon restrictions though. just do not hold me liable if it does not remind you of your important medication. it has multi-language support and handles reminder permissions. i figured out that reminders will not be pushed belated if the device is temporarily not connected. also changing account in the companion app will result in stopping reminders even if they are still visible.
+<img src="assets/forgetmenot108.png" alt="remind me skill logo" style="float:left; height:50px; padding-right:1em;" /> *forget-me-not / vergissmeinnicht* might actually be useful since it can remind you regulary for custom things. it fights with some silly amazon restrictions though. just do not hold me liable if it does not remind you of your important medication. it has multi-language support and handles reminder permissions. i figured out that reminders might not be pushed belated if the device is temporarily not connected. also changing accounts in the companion app will result in stopping reminders even if they are still visible.
 
 no special interface settings needed
